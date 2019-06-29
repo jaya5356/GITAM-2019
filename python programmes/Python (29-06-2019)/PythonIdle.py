bee066fed20784dab26181adcfc923d9b03bf5c3
jaya5356 <52007704+jaya5356@users.noreply.github.com>
2019-06-29 13:46:17 +0530

    Add files via upload

---
+++ python programmes/Python (29-06-2019)/PythonIdle.py
@@ -0,0 +1,8 @@
+## Step 1: Make all the turtles package to be imported  
+import turtle as tt
+## Turtle method creates and returns to a new object
+a1=tt.Turtle()
+## forward () moves 100 pixels
+tt.forward(100)
+## WE are done
+tt.done()
---
+++ python programmes/Python (29-06-2019)/pythonIdle6.py
@@ -0,0 +1,16 @@
+import turtle as tt
+
+
+c1 =tt.Turtle()
+c1.pencolor("blue")
+for i in range(50):
+    c1.forward(50)
+    c1.left(123)
+
+c1.pencolor("red")
+for i in range(red):
+    c1.forwards(100)
+    c1.left(123)
+
+tt.done()
+
